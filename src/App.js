8c7f6cf1667e8f7a509449d105cf6e6e652b1b5f
Callie Stansbury <calliestansbury@gmail.com>
2020-03-10 10:04:06 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Tabs from './Components/Tabs';
+import DisplayTab from './Components/DisplayTab'; 
 import './App.css';
 
 function App() {
+  const [setIndex, setIndexState] = useState(0)
+
+  const changeValue = (textIndex) => {
+    setIndexState(textIndex)
+  }
+
+  const textArray = [
+    "Puppies are the cutest and the best!",
+    "Kitties are cute but gross!",
+    "Bunnies have floppy ears and hop!",
+    "Fishies are wet and they swim!",
+    "Birdies have wings and sing to you!"
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs changeValue={() => changeValue(0)} tabLabel="Puppies" />
+      <Tabs changeValue={() => changeValue(1)} tabLabel="Kitties" />
+      <Tabs changeValue={() => changeValue(2)} tabLabel="Bunnies" />
+      <Tabs changeValue={() => changeValue(3)} tabLabel="Fishies" />
+      <Tabs changeValue={() => changeValue(4)} tabLabel="Birdies" />
+
+      <p><DisplayTab displayText={textArray[setIndex]} /></p>
+
     </div>
   );
 }
